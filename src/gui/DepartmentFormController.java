0b5f8a9b2556f1a0c568ae1a01a78dd4a4424434
João Pedro <jpedrocguimaraes@hotmail.com>
2022-10-03 16:30:38 -0300

    Implementado Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -60,6 +63,8 @@ public class DepartmentFormController implements Initializable {
 			Utils.currentStage(event).close();
 		} catch(DbException e) {
 			Alerts.showAlert("Erro ao salvar departamento", null, e.getMessage(), AlertType.ERROR);
+		} catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
 		}
 	}
 	
@@ -82,9 +87,27 @@ public class DepartmentFormController implements Initializable {
 	
 	private Department getFormData() {
 		Department department = new Department();
+		ValidationException exception = new ValidationException("Erro de validação");
+		
 		department.setId(Utils.tryParseToInt(txtId.getText()));
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "O campo não pode estar vazio");
+		}
 		department.setName(txtName.getText());
+		
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
 		return department;
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
 	}
 	
 	public void updateFormData() {
